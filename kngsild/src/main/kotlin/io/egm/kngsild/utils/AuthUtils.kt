355941a07016991f41971ed3b340a5411658fa82
Gaël Poujol <76944966+gpoujol@users.noreply.github.com>
2022-10-18 18:48:36 +0200

    feat: create AuthUtils class with provided token (#31)

--- kngsild/src/main/kotlin/io/egm/kngsild/utils/AuthUtils.kt
+++ kngsild/src/main/kotlin/io/egm/kngsild/utils/AuthUtils.kt
@@ -3,9 +3,7 @@ package io.egm.kngsild.utils
 import arrow.core.Either
 import arrow.core.left
 import arrow.core.right
-import io.egm.kngsild.model.AccessTokenNotRetrieved
-import io.egm.kngsild.model.ApplicationError
-import io.egm.kngsild.model.AuthenticationServerError
+import io.egm.kngsild.model.*
 import io.egm.kngsild.utils.HttpUtils.buildFormDataFromMap
 import io.egm.kngsild.utils.HttpUtils.httpClient
 import org.slf4j.LoggerFactory
@@ -14,52 +12,80 @@ import java.net.URI
 import java.net.http.HttpRequest
 import java.net.http.HttpResponse
 
-class AuthUtils(
-    private val serverUrl: String,
-    private val clientId: String,
-    private val clientSecret: String,
-    private val grantType: String,
-    private val enabled: Boolean = true
-) {
+class AuthUtils {
+    private var authType: AuthType = AuthType.NONE
+    private var providedToken: ProvidedToken? = null
+    private var clientCredentials: ClientCredentials? = null
+    constructor(providedToken: ProvidedToken){
+        this.providedToken = providedToken
+        this.authType = AuthType.PROVIDED_TOKEN
+    }
+
+    constructor(clientCredentials: ClientCredentials){
+        this.clientCredentials = clientCredentials
+        this.authType = AuthType.CLIENT_CREDENTIALS
+    }
 
     private val logger = LoggerFactory.getLogger(javaClass)
 
     fun getToken(): Either<ApplicationError, String> {
-        if (!enabled) {
-            logger.debug("Authentication is not enabled, returning random string")
-            return "Unused-Thing".right()
-        }
+        when (authType) {
+            AuthType.NONE -> {
+                logger.debug("Authentication is not enabled, returning random string")
+                return "Unused-Thing".right()
+            }
+            AuthType.PROVIDED_TOKEN -> return providedToken!!.accessToken.right()
 
-        val request = HttpRequest
-            .newBuilder()
-            .uri(URI.create(serverUrl))
-            .POST(
-                buildFormDataFromMap(
-                    mapOf(
-                        "client_id" to clientId,
-                        "client_secret" to clientSecret,
-                        "grant_type" to grantType
+            AuthType.CLIENT_CREDENTIALS -> {
+                val request = HttpRequest
+                    .newBuilder()
+                    .uri(URI.create(clientCredentials!!.serverUrl))
+                    .POST(
+                        buildFormDataFromMap(
+                            mapOf(
+                                "client_id" to clientCredentials!!.clientId,
+                                "client_secret" to clientCredentials!!.clientSecret,
+                                "grant_type" to clientCredentials!!.grantType
+                            )
+                        )
                     )
-                )
-            )
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .build()
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .build()
 
-        return try {
-            val httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString()).body()
-            val response: Map<String, String> = JsonUtils.mapper.readValue(
-                httpResponse,
-                JsonUtils.mapper.typeFactory.constructMapLikeType(
-                    Map::class.java, String::class.java, String::class.java
-                )
-            )
-            val accessToken = response["access_token"]
+                return try {
+                    val httpResponse = httpClient.send(request, HttpResponse.BodyHandlers.ofString()).body()
+                    val response: Map<String, String> = JsonUtils.mapper.readValue(
+                        httpResponse,
+                        JsonUtils.mapper.typeFactory.constructMapLikeType(
+                            Map::class.java, String::class.java, String::class.java
+                        )
+                    )
+                    val accessToken = response["access_token"]
 
-            accessToken?.right() ?: AccessTokenNotRetrieved("Unable to get an access token").left()
-        } catch (e: IOException) {
-            val errorMessage = e.message ?: "Error encountered while requesting token from authentication server"
-            logger.warn(errorMessage)
-            AuthenticationServerError(errorMessage).left()
+                    accessToken?.right() ?: AccessTokenNotRetrieved("Unable to get an access token").left()
+                } catch (e: IOException) {
+                    val errorMessage =
+                        e.message ?: "Error encountered while requesting token from authentication server"
+                    logger.warn(errorMessage)
+                    AuthenticationServerError(errorMessage).left()
+                }
+            }
         }
     }
 }
+
+enum class AuthType {
+    NONE,
+    PROVIDED_TOKEN,
+    CLIENT_CREDENTIALS,
+}
+
+data class ClientCredentials(
+    val serverUrl: String,
+    val clientId: String,
+    val clientSecret: String
+) {
+    val grantType = "client_credentials"
+}
+
+data class ProvidedToken(val accessToken: String)
--- kngsild/src/test/kotlin/io/egm/kngsild/utils/AuthUtilsTest.kt
+++ kngsild/src/test/kotlin/io/egm/kngsild/utils/AuthUtilsTest.kt
@@ -10,15 +10,11 @@ import com.github.tomakehurst.wiremock.client.WireMock.reset
 import com.github.tomakehurst.wiremock.client.WireMock.stubFor
 import com.github.tomakehurst.wiremock.client.WireMock.urlMatching
 import com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig
-import io.egm.kngsild.model.AccessTokenNotRetrieved
+import io.egm.kngsild.model.*
 import io.egm.kngsild.utils.JsonUtils.serializeObject
-import org.junit.jupiter.api.AfterAll
-import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.*
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
-import org.junit.jupiter.api.BeforeAll
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestInstance
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class AuthUtilsTest {
@@ -44,19 +40,33 @@ class AuthUtilsTest {
     }
 
     @Test
-    fun `it should return an access token`() {
+    fun `it should return an access token when client credentials mode is configured`() {
         stubFor(
             post(urlMatching("/auth"))
                 .willReturn(
                     ok().withBody(serializeObject(mapOf("access_token" to "token")))
                 )
         )
-
-        val response = AuthUtils(
+        val clientCredentials = ClientCredentials(
             "http://localhost:8089/auth",
             "client_id",
-            "client_secret",
-            "client_credentials"
+            "client_secret"
+        )
+
+        val response = AuthUtils(
+            clientCredentials = clientCredentials
+        ).getToken()
+
+        assertTrue(response.isRight())
+        assertEquals(response, "token".right())
+    }
+
+    @Test
+    fun `it should return an access token when ProvidedToken mode is configured`() {
+        val providedToken = ProvidedToken("token")
+
+        val response = AuthUtils(
+            providedToken = providedToken
         ).getToken()
 
         assertTrue(response.isRight())
@@ -69,12 +79,14 @@ class AuthUtilsTest {
             post(urlMatching("/auth"))
                 .willReturn(ok().withBody(serializeObject(emptyMap<String, Any>())))
         )
-
-        val response = AuthUtils(
+        val clientCredentials = ClientCredentials(
             "http://localhost:8089/auth",
             "client_id",
-            "client_secret",
-            "client_credentials"
+            "client_secret"
+        )
+
+        val response = AuthUtils(
+            clientCredentials = clientCredentials
         ).getToken()
 
         assertTrue(response.isLeft())
